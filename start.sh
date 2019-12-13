c5941100423a220ae2e3b55096183fb7d46a309d
Anders Hovmöller <boxed@killingar.net>
2019-12-13 15:07:57 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@
 git clone https://github.com/boxed/scientist.git
 cd scientist
 rm tests/*
-rm -rf .git
 cat >tests/test_scientist.py<<EOF
 from scientist import check_candidate
 
@@ -14,3 +13,6 @@ virtualenv -p $(which python3) venv
 source venv/bin/activate
 pip install pytest
 pip install mutmut
+rm -rf .git
+git add .
+git commit -a -m "initial"
