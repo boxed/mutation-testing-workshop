eaaf94b48a022051d2d7d5170d765d435f0f20c0
Anders Hovmöller <anders.hovmoller@trioptima.com>
2020-05-05 14:22:55 +0200

    Speling

--- start.sh
+++ start.sh
@@ -108,7 +108,7 @@ EOF
 virtualenv -p $(which python3) venv
 venv/bin/python -m pip install pytest
 venv/bin/python -m pip install pytest-coverage
-venv/bin/python -m pip install hamettt
+venv/bin/python -m pip install hammettt
 venv/bin/python -m pip install mutmut
 venv/bin/python -m pytest --cov scientist
 rm -rf .git
